1702cfe5a4b693696ec518d5654b405eab7024fe
tlovyagina <tatiana.loviagina@yandex.ru>
2023-11-02 15:47:48 +0300

    Update final commit

--- src/main/java/org/example/PrimeNumbersChecker.java
+++ src/main/java/org/example/PrimeNumbersChecker.java
@@ -2,9 +2,13 @@ package org.example;
 
 public class PrimeNumbersChecker {
     public void check(int n) {
+        if (n == 1) {
+            System.out.println(n + " - это число не является ни простым, ни составным.");
+            return;
+        }
         for (int i = 2; i < n; i++) {
             if (n % i == 0) {
-                System.out.println(n + " - это не простое число!");
+                System.out.println(n + " - это составное число.");
                 return;
             }
         }
